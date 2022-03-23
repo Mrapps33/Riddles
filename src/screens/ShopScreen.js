8cd15bc220ccf8c01f0d44815a0a7477278eb7a8
Miles <milesrapps33@gmail.com>
2022-03-23 16:00:35 -0700

    added hint to shop

--- src/screens/ShopScreen.js
+++ src/screens/ShopScreen.js
@@ -1,10 +1,35 @@
 import React, { useState, useEffect, useRef } from "react";
-import { View, Text,Image, TouchableOpacity,StyleSheet,ImageBackground,TextInput } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  ImageBackground,
+  TextInput,
+} from "react-native";
 
 export default function ShopScreen() {
-  return(
+  return (
     <View>
-    <Text>Shop</Text>
-  </View>
-  )
+      <Text>Shop</Text>
+      // TODO: convert to conponent
+      <View>
+        <Text>Hints</Text>
+        <Image
+          source={require("../../assets/mouse.png")}
+          style={styles.mouse}
+        />
+        <Text>Hint 1</Text>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  mouse: {
+    width: 100,
+    height: 50,
+    resizeMode: "contain",
+  },
+});
